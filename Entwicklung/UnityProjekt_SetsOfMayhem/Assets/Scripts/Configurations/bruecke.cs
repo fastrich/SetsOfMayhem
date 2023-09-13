dde190fe1e412f676078d0ba5222e39c36f6487e
Stefan Richter <o-s-r1993@web.de>
2023-09-13 09:01:27 +0200

    Added bridge file

---
+++ Entwicklung/UnityProjekt_SetsOfMayhem/Assets/Scripts/Configurations/bruecke.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static config_parameters;
+using static medium;
+using static config_parameters;
+
+public static class bruecke {
+
+	
+}
