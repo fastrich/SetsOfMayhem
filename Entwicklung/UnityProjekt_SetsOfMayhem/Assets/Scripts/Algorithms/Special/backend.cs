d48dd4ea8c99c1ae879532789696435322988143
Stefan Richter <o-s-r1993@web.de>
2023-09-14 14:04:46 +0200

    repaired the fresh backend

--- Entwicklung/UnityProjekt_SetsOfMayhem/Assets/Scripts/Algorithms/Special/backend.cs
+++ Entwicklung/UnityProjekt_SetsOfMayhem/Assets/Scripts/Algorithms/Special/backend.cs
@@ -1,5 +1,12 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 // sadly, this does not compile because of primitive vs complexe data types
-using Card = uint[];
+//using Card = uint[];
+using Card = System.Collections.Generic.List<uint>;
+
 
 public enum GameState : uint {
 	Init,       // game not initialized yet, call init() first
@@ -29,10 +36,10 @@ public class Game {
 		currentSize = 0;
 		maximumSize = defaultSize*2;
 		cardsLeft = m^n;
-		field = new uint[maximumSize];
-		cards = new uint[cardsLeft][n];
+		uint[] field = new uint[maximumSize];
+		uint[,] cards = new uint[cardsLeft,n];
 		helpSET = null;
-		state = Init;
+		state = GameState.Init;
 	}
 
 	public GameState getGameState() {
@@ -48,32 +55,32 @@ public class Game {
 		for (uint depth = 0; depth < N; depth++) {
 			uint c = 0;
 			for (uint i = 0; i < cardsLeft; i++) {
-				cards[i][depth] = c;
+				cards[i][(int)depth] = c;
 				if ((i+1) % (M^depth) == 0) {
 					c++;
 				}
 			}
 		}
 		// shuffle cards
-		Random rand = new Random();
-		uint c = cardsLeft;
-		while (c > 1) {
-			uint k = rand.Next(c--);
-			Card tmp = cards[c];
-			cards[c] = cards[k];
+		System.Random rand = new System.Random();
+		int c2 = (int)cardsLeft;
+		while (c2 > 1) {
+			int k = rand.Next(c2--);
+			Card tmp = cards[c2];
+			cards[c2] = cards[k];
 			cards[k] = tmp;
 		}
 	}
 
 	public GameState init() {
-		if (N < M) return Error;
+		if (N < M) return GameState.Error;
 
 		generateCards();
 		// initially fill field
 		for (uint i = 0; i < defaultSize; i++) {
 			field[currentSize++] = cards[--cardsLeft];
 		}
-		return (fillField() ? Error : Ready);
+		return (fillField() ? GameState.Error : GameState.Ready);
 	}
 
 	public bool isSET(Card[] set) {
@@ -82,9 +89,11 @@ public class Game {
 		uint[] feature = new uint[M];
 		for (uint n = 0; n < N; n++) {
 			for (uint m = 0; m < M; m++) {
-				feature[m] = set[m][n];
+				feature[m] = set[m][(int)n];
 			}
-			feature.sort();
+			
+			//feature.sort();
+			Array.Sort(feature);
 			bool equal = false;
 			bool different = false;
 			uint tmp = feature[0];
@@ -108,7 +117,7 @@ public class Game {
 
 		for (uint f = indexField; f < currentSize; f++) {
 			set[indexSET] = field[f];
-			maybeSET = findSEThelper(indexSET+1, f+1, set);
+			Card[] maybeSET = findSEThelper(indexSET+1, f+1, set);
 			if (maybeSET != null) {
 				return maybeSET;
 			}
@@ -117,13 +126,16 @@ public class Game {
 	}
 
 	private Card[] findSET() {
-		return findSEThelper(0, 0, new uint[M][]);
+		//return findSEThelper(0, 0, new uint[M][]);
+		Card[] nc = new Card[M];
+		return findSEThelper(0, 0, nc);
 	}
 
 	// return true if game is won
 	private bool fillField() {
 		while (true) {
-			uint[][] SET = findSET();
+			//uint[,] SET = findSET();
+			Card[] SET = findSET();
 			if (SET != null) {
 				helpSET = SET;
 				return false;
@@ -144,7 +156,7 @@ public class Game {
 	// argument: user input indices of possible SET
 	// return state after processing field
 	public GameState step(uint[] iSET) {
-		if (iSET.Length != M) return Again;
+		if (iSET.Length != M) return GameState.Again;
 
 		bool allEqual = true;
 		uint tmp = iSET[0];
@@ -154,24 +166,27 @@ public class Game {
 			if (iSET[m] != tmp) allEqual = false;
 			tmp = iSET[m];
 		}
-		if (allEqual) return Again;
-		if (!isSET(set)) return Again;
-
-		iSET.sort();
-		iSET.reverse();
+		if (allEqual) return GameState.Again;
+		if (!isSET(set)) return GameState.Again;
+		//iSET.sort();
+		Array.Sort(iSET);
+		//iSET.reverse();
+		Array.Reverse(iSET);
 		if (currentSize > defaultSize || cardsLeft < M) {
-			for i in iSET {
-				if (i != --currentSize) {
+			//for(i in iSET {
+			for (int i=0; i< iSET.Length; i++) {
+					if (i != --currentSize) {
 					field[i] = field[currentSize];
 				}
 			}
 		} else {
-			for i in iSET {
+			for (int i = 0; i < iSET.Length; i++)
+			{
 				field[i] = cards[--cardsLeft];
 			}
 		}
 		
-		return (fillField() ? Won : Ready);
+		return (fillField() ? GameState.Won : GameState.Ready);
 	}
 
 }
