ffb47a56e999d1ed992433fba238f8d97e5a42c5
Stefan Richter <o-s-r1993@web.de>
2023-09-14 14:26:36 +0200

    repaired the fresh backend

---
+++ Entwicklung/UnityProjekt_SetsOfMayhem/Assets/Scripts/Algorithms/Special/player2.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static medium;
+using static config_parameters;
+using static bruecke;
+using System;
+using static methods_unity;
+using static algos;
+
+public static class player2
+{
+    public static void player2_main(){
+
+
+    }
+
+    public static void player2_thereIsNewField()
+    {
+        checkForSetInField(0,true);
+
+        randomisePosOfMySelCards();
+        loadMyField();
+
+
+
+    }
+    public static void randomisePosOfMySelCards()
+    {
+        int quelle = 3;
+        int xtr = 0;//UnityEngine.Random.Range(0,4);
+        int i2 = 0;
+        int save1 = 0;
+        for (int i = 0; i < array_cards_selected_LengthIt(2, 1); i++)
+        {
+            
+
+            xtr = UnityEngine.Random.Range(0, array_cards_selected_LengthIt(2, 1));
+            i2 = i + xtr;
+            if (i2 >= array_cards_selected_LengthIt(2, 1))
+            {
+                i2 = i2 % array_cards_selected_LengthIt(2, 1);
+            }
+            save1 = array_cards_selected_GetIt(quelle, i2);
+            array_cards_selected_SetIt(quelle, i2, array_cards_selected_GetIt(quelle, i));
+            array_cards_selected_SetIt(quelle, i, save1);
+        }
+    }
+
+    public static void loadMyField()
+    {
+        int quelle = 0;
+        int quelle_arrsel = 3;
+        int ziel = 4;
+        int ziel_arrsel = 2;
+        //Debug.Log("hoho");
+        for (int i = 0; i < array_cards_selected_LengthIt(2, 1); i++)
+        {
+            array_cards_selected_SetIt(ziel_arrsel, i, array_cards_selected_GetIt(quelle_arrsel, i));
+            array_cards_status_SetIt(ziel, i, 5);
+            for (int ik = 0; ik < fieldOfCards_LengthIt(2, 2); ik++)
+            {
+                fieldOfCards_SetIt(ziel, i, ik, fieldOfCards_GetIt(quelle, array_cards_selected_GetIt(quelle_arrsel, i), ik));
+            }
+
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Entwicklung/UnityProjekt_SetsOfMayhem/Assets/Scripts/MonoBehaviors/Menue/internalScenesManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static methods_unity;
+using UnityEngine.SceneManagement;
+using static config_parameters;
+using static medium;
+using static algos;
+using static methods;
+using TMPro;
+public class internalScenesManager : MonoBehaviour
+{
+   public GameObject mainMenue;
+    public GameObject game_1;
+
+
+    private void Awake()
+    {
+        changeInternalScenesOff();
+        ChangeInternalScene("MainMenue");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+        
+    }
+
+
+    public void changeInternalScenesOff()
+    {
+        mainMenue.SetActive(false);
+        game_1.SetActive(false);
+    }
+
+    public void ChangeInternalScene(string Szene)
+    {
+        changeInternalScenesOff();
+        if (Szene.CompareTo("MainMenue")==0) { mainMenue.SetActive(true); }
+        if (Szene.CompareTo("Stage")==0) { game_1.SetActive(true); }
+    }
+    public void ChangeScene(string Szene)
+    {
+        SceneManager.LoadScene(sceneName: Szene); 
+    }
+
+
+
+
+}
